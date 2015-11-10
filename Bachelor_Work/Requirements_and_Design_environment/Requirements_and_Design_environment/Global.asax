1a7cb0bea968d70c36899ef9e54c5af381b09655
Keremidko <keremidko1990@abv.bg>
2015-11-10 10:31:42 +0200

    Deleted some files

--- Bachelor_Work/Requirements_and_Design_environment/Requirements_and_Design_environment/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Requirements_and_Design_environment.WebApiApplication" Language="C#" %>
--- Bachelor_Work/Requirements_and_Design_environment/Requirements_and_Design_environment/Scripts/controllers/ToolboxController.js
+++
@@ -1,7 +0,0 @@
-﻿RDE.controller("ToolboxController",
-    function ($scope, Tools) {
-        $scope.setTool = function (t) {
-            Tools.setTool(t);
-        }
-        $scope.tool = Tools.getTool();
-    });
